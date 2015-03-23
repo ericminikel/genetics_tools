7eb92e4c4f50467fd44a4b9ae397399b4ea1a6ad
Eric Minikel <ericminikel@gmail.com>
2015-03-23 10:11:33 -0400

    now navigates directory instead of zip file; you have to extract first.

--- align_traces.py
+++ align_traces.py
@@ -4,19 +4,19 @@ from Bio import SeqIO
 import sys
 import zipfile as zf
 import tempfile as tf
+import os
 
 '''
-Accepts a path to a zip file full of .ab1 traces
+Accepts a path to a directory full of .ab1 traces
 and returns a dictionary mapping name to sequence
 '''
-def get_traces(zip_path):
-    with zf.ZipFile(zip_path,mode='rb') as z:
-        zipped_items = z.infolist()
-        for item in zipped_items:
-            with z.open(item,mode='r') as ab1:
-                trace = SeqIO.parse(ab1,"abi").next()
-                seq = str(trace.seq)
-                print seq
+def get_traces(directory):
+    items = os.listdir(directory)
+    for item in items:
+        with open(os.path.join(directory,item),mode='rb') as ab1:
+            trace = SeqIO.parse(ab1,"abi").next()
+            seq = str(trace.seq)
+            print seq
 
 if __name__ == '__main__':
     get_traces(sys.argv[1])
